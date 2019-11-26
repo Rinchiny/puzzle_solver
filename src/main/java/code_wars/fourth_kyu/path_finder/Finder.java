2b9e4f52bfc4b7ca076d891608f403e587da3ff2
Rinchin <rinchin@simpls.ru>
2019-11-26 13:59:51 +0500

    link fix

--- src/main/java/code_wars/fourth_kyu/path_finder/Finder.java
+++ src/main/java/code_wars/fourth_kyu/path_finder/Finder.java
@@ -3,7 +3,7 @@ package code_wars.fourth_kyu.path_finder;
 import java.util.List;
 
 /**
- * This class provides solution for a puzzle described <a href="https://www.codewars.com/kata/path-finder-number-2-shortest-path/train/java">here</a>
+ * This class provides solution for a puzzle described <a href="https://www.codewars.com/kata/path-finder-number-1-can-you-reach-the-exit/train/java">here</a>
  */
 public class Finder {
 
