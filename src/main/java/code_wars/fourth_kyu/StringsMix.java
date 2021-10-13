c3adc2dad42f898908027b05eb35d5a4aac00254
Rinchiny <rinchiny@gmail.com>
2021-10-13 17:34:28 +0500

    updated 4th kyu "String mix" kata

--- src/main/java/code_wars/fourth_kyu/StringsMix.java
+++ src/main/java/code_wars/fourth_kyu/StringsMix.java
@@ -10,15 +10,11 @@ public class StringsMix {
 
 
     public String mix(String s1, String s2) {
+        var stringAsFilteredSortedList1 = convertToFilteredSortedList(s1);
+        var stringAsFilteredSortedList2 = convertToFilteredSortedList(s2);
 
-        var charArray1 = convertToCharArray(s1);
-        var charArray2 = convertToCharArray(s2);
-
-        var semiResult1 = convertToFilteredSortedList(charArray1);
-        var semiResult2 = convertToFilteredSortedList(charArray2);
-
-        var rawMap1 = convertToFilteredMap(semiResult1);
-        var rawMap2 = convertToFilteredMap(semiResult2);
+        var rawMap1 = convertToFilteredMap(stringAsFilteredSortedList1);
+        var rawMap2 = convertToFilteredMap(stringAsFilteredSortedList2);
 
         var editMap1 = convertToStringMap(rawMap1);
         var editMap2 = convertToStringMap(rawMap2);
@@ -42,17 +38,9 @@ public class StringsMix {
         return result;
     }
 
-    private static Character[] convertToCharArray(String s) {
-        char[] charArray = s.toCharArray();
-        Character[] characterArray = new Character[charArray.length];
-        for (int i = 0; i < charArray.length; i++) {
-            characterArray[i] = charArray[i];
-        }
-        return characterArray;
-    }
-
-    private static List<Character> convertToFilteredSortedList(Character[] charArray) {
-        return Arrays.stream(charArray)
+    private static List<Character> convertToFilteredSortedList(String string) {
+        return string.chars()
+                .mapToObj(i -> (char)i)
                 .filter(Character::isLetter)
                 .filter(Character::isLowerCase)
                 .sorted()
@@ -60,7 +48,7 @@ public class StringsMix {
     }
 
     private static Map<Character, Integer> convertToFilteredMap(List<Character> list) {
-        Map<Character, Integer> numberOfOccurrences = new HashMap<>();
+        var numberOfOccurrences = new HashMap<Character, Integer>();
         list.forEach(character -> {
             int occurrences = Collections.frequency(list,character);
             numberOfOccurrences.put(character,occurrences);
@@ -80,10 +68,7 @@ public class StringsMix {
     }
 
     private static Map<Character, String> addSymbolsToMap(Map<Character, String> rawMap, int stringNumber) {
-        for (Map.Entry<Character, String> entry : rawMap.entrySet()) {
-            rawMap.put(entry.getKey(), String.valueOf(stringNumber)+":"+entry.getValue()+"/");
-        }
-
+        rawMap.replaceAll((k, v) -> stringNumber + ":" + v + "/");
         return rawMap;
     }
 
@@ -110,18 +95,8 @@ public class StringsMix {
     }
 
     private static List<String> lexicographicOrder(List<String> nonOrderedList) {
-        Collections.sort(nonOrderedList, new Comparator<String>() {
-            @Override
-            public int compare(String o1, String o2) {
-                return o1.compareTo(o2);
-            }
-        });
-        Collections.sort(nonOrderedList, new Comparator<String>() {
-            @Override
-            public int compare(String o1, String o2) {
-                return Integer.compare(o2.length(),o1.length());
-            }
-        });
+        nonOrderedList.sort(String::compareTo);
+        nonOrderedList.sort((o1, o2) -> Integer.compare(o2.length(), o1.length()));
         return nonOrderedList;
     }
 }
