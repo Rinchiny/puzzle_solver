e14c8b01d26b2330c2512f44171623a0bb43bb33
Rinchiny <rinchiny@gmail.com>
2022-01-10 17:16:14 +0500

    solved LeetCode easy "Two sum" problem

---
+++ src/main/java/leet_code/easy/TwoSum.java
@@ -0,0 +1,35 @@
+package leet_code.easy;
+
+import java.util.HashMap;
+
+/**
+ * This class provides solution for a puzzle described <a href="https://leetcode.com/problems/two-sum/">here</a>
+ */
+public class TwoSum {
+
+    public int[] twoSumNaive(int[] nums, int target) {
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
+                if (nums[j] == target - nums[i]) {
+                    return new int[] { i, j };
+                }
+            }
+        }
+        return null;
+    }
+
+    public int[] twoSumMemoization(int[] nums, int target) {
+        var map = new HashMap<Integer, Integer>();
+        for (int i = 0; i < nums.length; i++) {
+            map.put(nums[i], i);
+        }
+        for (int i = 0; i < nums.length; i++) {
+            var complement = target - nums[i];
+            if (map.containsKey(complement) && map.get(complement) != i) {
+                return new int[] { i, map.get(complement) };
+            }
+        }
+        // In case there is no solution, we'll just return null
+        return null;
+    }
+}
