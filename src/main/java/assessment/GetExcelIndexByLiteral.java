f6d67a8cc5d384c0a5877554cd9fa3f0a58f31ac
Rinchiny <rinchiny@gmail.com>
2022-01-21 17:28:15 +0300

    Added more code snippets on Base26 -> Base10

--- src/main/java/assessment/GetExcelIndexByLiteral.java
+++ src/main/java/assessment/GetExcelIndexByLiteral.java
@@ -10,4 +10,15 @@ public class GetExcelIndexByLiteral {
         }
         return result;
     }
+
+    public int getIndexByLiteral2(String literal) {
+        var charArray = literal.toCharArray();
+        var accum = 0;
+        for (int i = 0; i < charArray.length; i++) {
+            var c = charArray[charArray.length - 1 - i];
+            accum += ((c - 'A' + 1) * Math.pow(26, i));
+        }
+
+        return accum;
+    }
 }
--- src/test/java/assessment/GetExcelIndexByLiteralTest.java
+++ src/test/java/assessment/GetExcelIndexByLiteralTest.java
@@ -21,4 +21,18 @@ class GetExcelIndexByLiteralTest {
     void getIndexByLiteral2() {
         assertEquals(703, testSubject.getIndexByLiteral("AAA"));
     }
+
+    @Test
+    void getIndexByLiteral3() {
+        assertEquals(1, testSubject.getIndexByLiteral2("A"));
+        assertEquals(27, testSubject.getIndexByLiteral2("AA"));
+        assertEquals(28, testSubject.getIndexByLiteral2("AB"));
+        assertEquals(53, testSubject.getIndexByLiteral2("BA"));
+        assertEquals(702, testSubject.getIndexByLiteral2("ZZ"));
+    }
+
+    @Test
+    void getIndexByLiteral4() {
+        assertEquals(703, testSubject.getIndexByLiteral2("AAA"));
+    }
 }
