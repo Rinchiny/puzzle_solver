e53da62904b8cdf279d4314f7224ca09a43ec996
Rinchiny <rinchiny@gmail.com>
2022-04-19 10:17:58 +0500

    Solved LeetCode easy "Happy Number" problem

---
+++ src/main/java/leet_code/easy/HappyNumber.java
@@ -0,0 +1,29 @@
+package leet_code.easy;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.stream.Stream;
+
+/**
+ * This class provides solution for a puzzle described <a href="https://leetcode.com/problems/happy-number/">here</a>
+ */
+public class HappyNumber {
+
+    private Set<Integer> stor = new HashSet<>();
+
+    public boolean isHappy(int n) {
+        if (n == 1)
+            return true;
+        if (stor.contains(n))
+            return false;
+        stor.add(n);
+        var accum = 0;
+        while (n > 0) {
+            var m = n % 10;
+            accum+= m * m;
+            n = n / 10;
+        }
+        return isHappy(accum);
+    }
+}
