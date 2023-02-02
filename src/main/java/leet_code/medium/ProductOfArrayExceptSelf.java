83587d1912b0838ddc6b5e34c9b4114d47bc259e
Rinchiny <rinchiny@gmail.com>
2023-02-02 13:12:53 +0100

    Cleaned up LeetCode medium "Product of Array Except Self" problem

--- src/main/java/leet_code/medium/ProductOfArrayExceptSelf.java
+++ src/main/java/leet_code/medium/ProductOfArrayExceptSelf.java
@@ -6,16 +6,16 @@ package leet_code.medium;
 public class ProductOfArrayExceptSelf {
 
     public int[] productExceptSelf(int[] nums) {
-        int n = nums.length;
-        int[] ans = new int[n];
-        for (int i = 0, left = 1; i < n; ++i) {
-            ans[i] = left;
+        var n = nums.length;
+        var answer = new int[n];
+        for (int i = 0, left = 1; i < n; i++) {
+            answer[i] = left;
             left *= nums[i];
         }
-        for (int i = n - 1, right = 1; i >= 0; --i) {
-            ans[i] *= right;
+        for (int i = n - 1, right = 1; i >= 0; i--) {
+            answer[i] *= right;
             right *= nums[i];
         }
-        return ans;
+        return answer;
     }
 }
